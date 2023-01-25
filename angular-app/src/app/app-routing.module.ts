553bf98e55d0f8567dccea6e5e98a6c00ed7f9e7
roshith p <roshith844@gmail.com>
2023-01-25 22:16:46 +0530

    feat: routes

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+
+const routes: Routes = [
+  {path:'login',component:  LoginComponent },
+  {path:'signup',component: SignupComponent },
+  {path:'',component: LandingComponent },
+  {path:'home',component:HomeComponent },
+
+  {path:'admin-login',component:AdminLoginComponent },
+  {path:'admin-home',component: AdminHomeComponent  },
+  {path:'user-profile',component: UserProfileComponent },
+  {path:'create-user',component: CreateUserComponent },
+
+  {path:'edit user',component: EditUserComponent  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
