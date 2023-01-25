1b5e4423aceac02a3ae99798466473bdb25f4642
roshith p <roshith844@gmail.com>
2023-01-25 21:34:43 +0530

    feat: boilerplate template

---
+++ angular-app/src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminLoginComponent } from './components/admin-login/admin-login.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
+import { CreateUserComponent } from './components/create-user/create-user.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    LandingComponent,
+    HomeComponent,
+    AdminLoginComponent,
+    AdminHomeComponent,
+    UserProfileComponent,
+    CreateUserComponent,
+    EditUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
