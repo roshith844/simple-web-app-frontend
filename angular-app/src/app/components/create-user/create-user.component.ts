99a331e3af8f570c7438b3392168fc7ba3d98c13
roshith p <roshith844@gmail.com>
2023-01-26 17:26:17 +0530

    feat: delete user from database

--- angular-app/src/app/components/create-user/create-user.component.ts
+++ angular-app/src/app/components/create-user/create-user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { BackendManagementService } from 'src/app/services/backend-management.service';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
@@ -7,7 +9,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class CreateUserComponent {
 // createUserForm
-constructor(private formBuilder: FormBuilder ){
+constructor(private formBuilder: FormBuilder,private apiService: BackendManagementService, private router: Router  ){
 
 }
 createUserForm = this.formBuilder.group({
@@ -18,6 +20,13 @@ createUserForm = this.formBuilder.group({
 
 onSubmit(){
   console.log(this.createUserForm.value)
+  this.apiService.addUser(this.createUserForm.value).subscribe((res)=>{
+    console.log(res)
+    // if(res.success == true){
+    //   localStorage.setItem('jwt', res.token)
+     this.router.navigate(['/admin-home'])
+    // }
+    })
   this.createUserForm.reset()
 }
 }
